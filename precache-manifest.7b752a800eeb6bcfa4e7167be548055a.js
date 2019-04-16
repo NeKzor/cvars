ec7d5970e589cb7af3365540930d5260e6c917b9
NeKzBot <44978126+NeKzBot@users.noreply.github.com>
2019-04-17 01:03:28 +0200

    Update

---
+++ precache-manifest.7b752a800eeb6bcfa4e7167be548055a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e6f2848a0a707d5acbf",
+    "url": "/cvars/static/js/runtime~main.d3ceab40.js"
+  },
+  {
+    "revision": "a4f6846efbc2afed7fbf",
+    "url": "/cvars/static/js/main.ad02b32e.chunk.js"
+  },
+  {
+    "revision": "66d15cba54a11a9fc527",
+    "url": "/cvars/static/js/2.74e0e712.chunk.js"
+  },
+  {
+    "revision": "01913fd09592ffe44b77235f163412db",
+    "url": "/cvars/index.html"
+  }
+];
