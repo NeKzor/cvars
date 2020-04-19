13251ff0fe0d8e8acbe69d4ef9e137dd1f0ad873
NeKz <nekzor@users.noreply.github.com>
2020-04-19 22:47:44 +0200

    Build without default features enabled

--- module/build.bat
+++ module/build.bat
@@ -3,7 +3,7 @@
 SET "binary=cvars.dll"
 SET "dest=C:\Program Files (x86)\Steam\SteamApps\common"
 
-cargo build --lib --release --target i686-pc-windows-msvc -Z unstable-options --out-dir ./bin/%1/ --features %1
+cargo build --lib --release --target i686-pc-windows-msvc -Z unstable-options --out-dir ./bin/%1/ --features %1 --no-default-features
 
 IF "%1" == "portal2" (
     COPY /Y ".\bin\portal2\%binary%" "%dest%\Portal 2\%binary%"
--- module/build_x64.bat
+++ module/build_x64.bat
@@ -3,4 +3,4 @@
 SET "binary=cvars.dll"
 SET "dest=C:\Program Files (x86)\Steam\SteamApps\common"
 
-cargo build --lib --release --target x86_64-pc-windows-msvc -Z unstable-options --out-dir ./bin/%1/ --features %1
+cargo build --lib --release --target x86_64-pc-windows-msvc -Z unstable-options --out-dir ./bin/%1/ --features %1 --no-default-features
