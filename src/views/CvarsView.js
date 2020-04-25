59d22094bdeeaa3bcc2db9f1908f4598c800e229
NeKz <nekzor@users.noreply.github.com>
2020-04-25 15:26:09 +0200

    Advanced query search

--- src/views/CvarsView.js
+++ src/views/CvarsView.js
@@ -20,12 +20,36 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const searchQuery = (query, match) => {
+    if (!query) return false;
+
+    const isArray = Array.isArray(match);
+
+    const values = query.split(',');
+    for (const value of values) {
+        if (isArray) {
+            for (const matchValue of match) {
+                if (value === matchValue) {
+                    return true;
+                }
+            }
+        } else if (value === match) {
+            return true;
+        }
+    }
+    return false;
+};
+
 const CvarsView = ({ hasNewCheckbox }) => {
     const isMounted = useIsMounted();
 
     const { pathname, search } = useLocation();
     const query = new URLSearchParams(search);
-    const defaultSearch = query.get('search');
+    const defaultName = query.get('name');
+    const defaultDefault = query.get('default');
+    const defaultFlags = query.get('flags');
+    const defaultSystem = query.get('system');
+    const defaultHelpText = query.get('help');
 
     const {
         state: { games },
@@ -35,9 +59,17 @@ const CvarsView = ({ hasNewCheckbox }) => {
     useTitle((game ? game.title + ' | ' : '') + 'Cvars');
 
     const [cvars, setCvars] = React.useState([]);
-    const [filter, setFilter] = React.useState(() => (cvar) =>
-        defaultSearch !== null ? cvar.name.includes(defaultSearch) : true,
-    );
+    const [filter, setFilter] = React.useState(() => (cvar) => {
+        if (!defaultName && !defaultDefault && !defaultFlags && !defaultSystem && !defaultHelpText) {
+            return true;
+        }
+
+        return searchQuery(defaultName, cvar.name)
+            || searchQuery(defaultDefault, cvar.default)
+            || searchQuery(defaultFlags, cvar.getFlags())
+            || searchQuery(defaultSystem, cvar.getOs())
+            || searchQuery(defaultHelpText, cvar.help);
+    });
 
     React.useEffect(() => {
         Api.get(pathname)
