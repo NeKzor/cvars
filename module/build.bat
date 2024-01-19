61335dd52513d8aa1b8c38388e302596169e0352
NeKz <nekzor@users.noreply.github.com>
2024-01-19 07:55:02 +0100

    Update scripts to build on stable Rust

--- module/build.bat
+++ module/build.bat
@@ -3,8 +3,8 @@
 SET "binary=cvars.dll"
 SET "dest=C:\Program Files (x86)\Steam\SteamApps\common"
 
-cargo build --lib --release --target i686-pc-windows-msvc -Z unstable-options --out-dir ./bin/%1/ --features %1 --no-default-features
+cargo build --lib --release --target i686-pc-windows-msvc --features %1 --no-default-features
 
 IF "%1" == "portal2" (
-    COPY /Y ".\bin\portal2\%binary%" "%dest%\Portal 2\%binary%"
+    COPY /Y ".\target\i686-pc-windows-msvc\release\%binary%" "%dest%\Portal 2\%binary%"
 )
--- module/build_x64.bat
+++ module/build_x64.bat
@@ -3,4 +3,8 @@
 SET "binary=cvars.dll"
 SET "dest=C:\Program Files (x86)\Steam\SteamApps\common"
 
-cargo build --lib --release --target x86_64-pc-windows-msvc -Z unstable-options --out-dir ./bin/%1/ --features %1 --no-default-features
+cargo build --lib --release --target x86_64-pc-windows-msvc --features %1 --no-default-features
+
+IF "%1" == "portal2_ce" (
+    COPY /Y ".\target\x86_64-pc-windows-msvc\release\%binary%" "%dest%\Portal Revolution\%binary%"
+)
